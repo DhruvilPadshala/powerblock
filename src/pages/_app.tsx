80b01155db945f1f965235591fd59d688ac96f96
dhruvil padshala <padshaladhruvil5@gmail.com>
2026-06-28 16:21:05 +0530

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      {/* Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-WC67D6XEE7"
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-WC67D6XEE7', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+    <><script async src="https://www.googletagmanager.com/gtag/js?id=G-WC67D6XEE7"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WC67D6XEE7');
+      </script>
       <Component {...pageProps} />
     </>
   );
