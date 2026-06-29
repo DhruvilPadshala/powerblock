939b2e0220a615ae48e0ac040752b8aa68a49d95
dhruvil padshala <padshaladhruvil5@gmail.com>
2026-06-29 11:43:44 +0530

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <><script async src="https://www.googletagmanager.com/gtag/js?id=G-WC67D6XEE7"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-WC67D6XEE7');
-      </script>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
-
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WC67D6XEE7" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WC67D6XEE7');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
